91455ba81b489da802676f35bd73b9f40d9fdfa3
dennis.zubov <denniszubov365@gmail.com>
2022-12-06 03:35:07 -0500

    Save playlists to DB and display from DB

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import secrets
 import env
 import time
 import boto3
+import datetime
+from boto3.dynamodb.conditions import Key, Attr
 
 
 app = Flask(__name__)
@@ -27,6 +29,7 @@ def dashboard():
         return redirect('/')
 
     sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    session["user_id"] = sp.current_user()["id"]
 
     # Get Recommendations based on Top Artists
     top_artists = sp.current_user_top_artists(time_range="short_term", limit=5)
@@ -34,12 +37,29 @@ def dashboard():
 
     recs_based_on_artists = sp.recommendations(limit=10, seed_artists=top_artist_ids)
     recs_artists = []
+    artist_ids = []
+    artist_uris = []
+    artist_songArtists = []
+    artist_songNames = []
+
     for idx, track_item in enumerate(recs_based_on_artists['tracks']):
         track = {
             "artist": track_item["artists"][0]["name"],
             "track_name": track_item["name"]
         }
+        artist_ids.append(track_item["id"])
+        artist_uris.append(track_item["uri"])
+        artist_songArtists.append(track_item["artists"][0]["name"])
+        artist_songNames.append(track_item["name"])
         recs_artists.append(track)
+        
+
+    # Save ids in session storage in case user saves this playlist
+    session["artist_ids"] = artist_ids
+    session["artist_uris"] = artist_uris
+    session["artist_songArtists"] = artist_songArtists
+    session["artist_songNames"] = artist_songNames
+
 
     # Get Recommendations based on Top Tracks
     top_tracks = sp.current_user_top_tracks(time_range="short_term", limit=5)
@@ -47,28 +67,57 @@ def dashboard():
     recs_based_on_tracks = sp.recommendations(seed_tracks=top_tracks_ids, limit=10)
 
     recs_tracks = []
+    track_ids = []
+    track_uris = []
+    track_songArtists = []
+    track_songNames = []
     for idx, track_item in enumerate(recs_based_on_tracks['tracks']):
         track = {
             "artist": track_item["artists"][0]["name"],
             "track_name": track_item["name"]
         }
+        track_ids.append(track_item["id"])
+        track_uris.append(track_item["uri"])
+        track_songArtists.append(track_item["artists"][0]["name"])
+        track_songNames.append(track_item["name"])
         recs_tracks.append(track)
+
+    # Save ids in session storage in case user saves this playlist
+    session["track_ids"] = track_ids
+    session["track_uris"] = track_uris
+    session["track_songArtists"] = track_songArtists
+    session["track_songNames"] = track_songNames
 
     # General Recommendations
     recs_based_on_both = sp.recommendations(seed_tracks=top_tracks_ids[:3], seed_artists=top_artist_ids[:2], limit=10)
     recs_general = []
+    general_ids = []
+    general_uris = []
+    general_songArtists = []
+    general_songNames = []
     for idx, track_item in enumerate(recs_based_on_both['tracks']):
         track = {
             "artist": track_item["artists"][0]["name"],
             "track_name": track_item["name"]
         }
+        general_ids.append(track_item["id"])
+        general_uris.append(track_item["uri"])
+        general_songArtists.append(track_item["artists"][0]["name"])
+        general_songNames.append(track_item["name"])
         recs_general.append(track)
+
+    # Save ids in session storage in case user saves this playlist
+    session["general_ids"] = general_ids
+    session["general_uris"] = general_uris
+    session["general_songArtists"] = general_songArtists
+    session["general_songNames"] = general_songNames
 
     data = {
         "recs_artists": recs_artists,
         "recs_tracks": recs_tracks,
         "recs_general": recs_general
     }
+    session["data"] = data
 
     return render_template("dashboard.html", data=data)
 
@@ -128,7 +177,158 @@ def saved():
     if not authorized():
         return redirect('/')
 
-    return render_template("saved-playlists.html")
+    user_id = session.get("user_id")
+    # Send to login if the user id is not in the session
+    if not user_id:
+        return redirect('/')
+    response = table.scan(FilterExpression=Attr('user').eq(user_id)).get("Items")
+    # print(response)
+    playlists = []
+    for pl in response:
+        track_names = pl["song-names"].split(",")
+        # print(track_names)
+        track_artists = pl["song-artists"].split(",")
+        # print(track_artists)
+        playlist = {
+            "tracks": [],
+            "id": pl["playlist-id"]
+        }
+        for name, artist in zip(track_names, track_artists):
+            playlist["tracks"].append(
+                {
+                    "name": name,
+                    "artist": artist
+                }
+            )
+        playlists.append(playlist)
+        
+    # print(playlists)
+    return render_template("saved-playlists.html", data={"playlists": playlists})
+
+
+@app.route('/save-general-playlist', methods=['GET'])
+def save_general_playlist():
+    if not authorized():
+        return redirect('/')
+
+    user_id = session.get("user_id")
+    # Send to login if the user id is not in the session
+    if not user_id:
+        return redirect('/')
+
+    # Save this playlist to dynamo db
+    general_ids = session.get("general_ids")
+    general_uris = session.get("general_uris")
+    general_songArtists = session.get("general_songArtists")
+    general_songNames = session.get("general_songNames")
+    song_ids = ",".join(general_ids)
+    song_uris = ",".join(general_uris)
+    song_artists = ",".join(general_songArtists)
+    song_names = ",".join(general_songNames)
+
+    table.put_item(
+        Item={
+            "playlist-id": "G" + str(int(datetime.datetime.now().strftime('%s'))),
+            "song-id": song_ids,
+            "song-uris": song_uris,
+            "user": user_id,
+            "song-artists": song_artists,
+            "song-names": song_names
+        }
+    )
+
+    return render_template("dashboard.html", data=session.get("data"))
+
+
+@app.route('/save-tracks-playlist', methods=['GET'])
+def save_tracks_playlist():
+    if not authorized():
+        return redirect('/')
+
+    user_id = session.get("user_id")
+    # Send to login if the user id is not in the session
+    if not user_id:
+        return redirect('/')
+
+    # Save this playlist to dynamo db
+    track_ids = session.get("track_ids")
+    track_uris = session.get("track_uris")
+    track_songArtists = session.get("track_songArtists")
+    track_songNames = session.get("track_songNames")
+    song_ids = ",".join(track_ids)
+    song_uris = ",".join(track_uris)
+    song_artists = ",".join(track_songArtists)
+    song_names = ",".join(track_songNames)
+
+    table.put_item(
+        Item={
+            "playlist-id": "T" + str(int(datetime.datetime.now().strftime('%s'))),
+            "song-id": song_ids,
+            "song-uris": song_uris,
+            "user": user_id,
+            "song-artists": song_artists,
+            "song-names": song_names
+        }
+    )
+
+    return render_template("dashboard.html", data=session.get("data"))
+
+
+@app.route('/save-artists-playlist', methods=['GET'])
+def save_artists_playlist():
+    if not authorized():
+        return redirect('/')
+
+    user_id = session.get("user_id")
+    # Send to login if the user id is not in the session
+    if not user_id:
+        return redirect('/')
+
+    # Save this playlist to dynamo db
+    artist_ids = session.get("artist_ids")
+    artist_uris = session.get("artist_uris")
+    artist_songArtists = session.get("artist_songArtists")
+    artist_songNames = session.get("artist_songNames")
+    song_ids = ",".join(artist_ids)
+    song_uris = ",".join(artist_uris)
+    song_artists = ",".join(artist_songArtists)
+    song_names = ",".join(artist_songNames)
+
+    table.put_item(
+        Item={
+            "playlist-id": "A" + str(int(datetime.datetime.now().strftime('%s'))),
+            "song-id": song_ids,
+            "song-uris": song_uris,
+            "user": user_id,
+            "song-artists": song_artists,
+            "song-names": song_names
+        }
+    )
+
+    return render_template("dashboard.html", data=session.get("data"))
+
+
+# Delete all saved playlists
+@app.route('/delete-saved-playlists', methods=["GET"])
+def delete_saved_playlists():
+    if not authorized():
+        return redirect('/')
+
+    user_id = session.get("user_id")
+    # Send to login if the user id is not in the session
+    if not user_id:
+        return redirect('/')
+
+    response = table.scan(FilterExpression=Attr('user').eq(user_id)).get("Items")
+    for item in response:
+        table.delete_item(
+            Key={
+                "playlist-id": item["playlist-id"],
+                "song-id": item["song-id"]
+            }
+        )
+
+    return render_template("saved-playlists.html", data=session.get("data"))
 
 
 @app.route('/about', methods=['GET'])
